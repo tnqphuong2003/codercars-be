b0cece51a0ec1dc6c910bd2893309ec8f2690968
tnqphuong2003 <tnqphuong2003@gmail.com>
2023-03-17 17:49:03 +0700

    update

--- app.js
+++ app.js
@@ -18,10 +18,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// Connect to MONGODB
-mongoose.connect(process.env.MONGO_URI, () => {
-  console.log("Connected to Database!");
-});
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI);
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 app.use("/", indexRouter);
 
